5b9bed6421d4a79a00c435f03d1900602bd9d981
Sammlana <jokolkkpk@gmail.com>
2024-05-13 20:04:31 +0800

    2

---
+++ database/connetion.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "Localhost";
+$username = "root";
+$password = "";
+$dbname = "penggajian";
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+if (!$connection){
+    die("Gagal Koneksi". mysqli_connect_error());
+}
