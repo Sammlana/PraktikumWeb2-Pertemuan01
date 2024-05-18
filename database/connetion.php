63ab243faa442dd695ac6b38b6218779daac3432
Sammlana <jokolkkpk@gmail.com>
2024-05-18 22:33:47 +0800

    penggajian

--- database/connetion.php
+++ database/connetion.php
@@ -2,7 +2,7 @@
 $servername = "Localhost";
 $username = "root";
 $password = "";
-$dbname = "penggajian";
+$dbname = "db_penggajian";
 
 $connection = mysqli_connect($servername, $username, $password, $dbname);
 if (!$connection){
